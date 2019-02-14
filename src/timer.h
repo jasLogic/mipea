cda0a810de491d41b9f59f0420b289a542b28276
jasLogic <44301923+jasLogic@users.noreply.github.com>
2019-02-15 00:24:18 +0100

    added system timer

---
+++ src/timer.h
@@ -0,0 +1,33 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define TIMER_BASE          0x20003000
+#define TIMER_BLOCK_SIZE    0x1C
+
+volatile uint32_t *timer_base_ptr;
+
+struct timer_register_map {
+    uint32_t CS;
+    uint32_t CLO;
+    uint32_t CHI;
+    uint32_t C0;
+    uint32_t C1;
+    uint32_t C2;
+    uint32_t C3;
+};
+#define TIMER     ((struct timer_register_map *)timer_base_ptr)
+
+uint32_t *  timer_map(void);
+void        timer_unmap(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _TIMER_H_ */
